cf5f20dd8779adf28f6731acba2b7c590361ba77
kbird <kimamy0902@gmail.com>
2024-03-26 16:37:31 +0900

    list에서 container 문서 id 넘기기

--- lib/community/screens/question_detail_screen.dart
+++ lib/community/screens/question_detail_screen.dart
@@ -4,25 +4,35 @@ import '../../common/themes/app_colors.dart' as app_colors;
 import 'package:intl/intl.dart';
 
 class QuestionDetailScreen extends StatefulWidget {
-  const QuestionDetailScreen({super.key});
+  final String docId; // 인덱스 매개변수 정의
+
+  const QuestionDetailScreen({super.key, required this.docId});
 
   @override
   State<QuestionDetailScreen> createState() => _QuestionDetailState();
 }
 
 class _QuestionDetailState extends State<QuestionDetailScreen> {
-  String questionTitle = "CSS의 가상 클래스와 가상 요소 달디 달디 달고 달디 달고 달디단 밤양갱";
-  String question = '아래 방식으로 설명해주세요! 달디 달디 달디 달디 달고 단 밤양갱갱ㄱ액액ㅇ개개액애';
-  String nickname = 'kimam******';
+  late String docId;
+  String questionTitle = "css가 뭔가요?";
+  String question = 'html과 어떤 관련이 있을까요??';
+  String nickname = 'goog******';
   String answerNickname = 'CodeClimX';
   String userAnswer =
-      '가상 클래스와 가상 요소는 HTML 요소의 특정 상태나 위치에 따라 스타일을 동적으로 변경하고 다양한 디자인 요구사항을 족시키는 데 사용됩니다.';
+      'CSS는 "Cascading Style Sheets"의 약자로, 웹 페이지의 디자인을 담당하는 스타일 시트 언어입니다. 이를 통해 HTML로 구축된 웹 페이지의 레이아웃, 컬러, 폰트 크기 등 다양한 시각적 요소를 정의하고 관리할 수 있습니다. HTML과 CSS는 밀접하게 연관되어 있습니다. HTML은 웹 페이지의 구조를 담당하는 반면, CSS는 그 구조에 스타일을 적용하는 역할을 합니다. 예를 들어, HTML을 사용해 텍스트나 이미지 등의 웹 콘텐츠를 웹 페이지에 배치하고, CSS를 사용해 이러한 콘텐츠의 배경색, 마진, 폰트 속성 등을 조절하여 시각적으로 더 매력적으로 만듭니다. HTML 요소에 CSS 스타일을 적용하는 방법에는 여러 가지가 있습니다. 가장 직접적인 방법은 HTML 태그 내에 `style` 속성을 사용하는 것이며, 좀 더 체계적으로 스타일을 관리하기 위해 `<head>` 태그 안에 `<style>` 태그를 사용하거나 외부 스타일 시트 파일을 만들어 링크할 수도 있습니다. CSS를 활용하면 웹 페이지의 일관성을 유지하면서도 유지보수를 용이하게 하며 사용자 경험을 향상시킬 수 있습니다.';
   String userStatus = 'AI';
   String postDate = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
 
   int like = 9283;
 
   bool isFavorited = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // initState에서 widget.index 값을 가져와 index 변수에 할당합니다.
+    docId = widget.docId;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -127,13 +137,13 @@ class _QuestionDetailState extends State<QuestionDetailScreen> {
               ),
             ),
             const SizedBox(height: 15),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Image.asset(
-                'assets/asdf.png',
-                fit: BoxFit.cover,
-              ),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.symmetric(horizontal: 30),
+            //   child: Image.asset(
+            //     'assets/asdf.png',
+            //     fit: BoxFit.cover,
+            //   ),
+            // ),
             const SizedBox(height: 50),
             Padding(
               padding: const EdgeInsets.only(left: 20), // 왼쪽에 20의 패딩을 추가합니다.
